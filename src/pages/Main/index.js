def32bcaca5a9860190684812e303a5fe2511643
andrioc <andrio.rodrigo.silva@hotmail.com>
2020-04-02 20:14:30 -0300

    navigation

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,22 @@
 import React, {Component} from 'react'
 import {Keyboard, ActivityIndicator} from 'react-native'
+import PropTypes from 'prop-types'
 import AsyncStorage from '@react-native-community/async-storage'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 import {Container, Form, Input, SubmitButton, List, User, Avatar,
 Name, Bio, ProfileButton, ProfileButtonText} from './styles'
 import api from '../../services/api'
 export default class Main extends Component{
+
+  static navigationOptions = {
+    title: 'Users'
+  }
+
+  static proptTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired
+  }
 
   state = {
     newUser: '',
@@ -57,6 +68,13 @@ export default class Main extends Component{
 
   }
 
+  handleNavigate = (user) => {
+    const {navigation} = this.props
+
+    navigation.navigate('User', {user})
+
+  }
+
   render() {
 
     const {users, newUser, loading} = this.state
@@ -88,7 +106,7 @@ export default class Main extends Component{
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -99,8 +117,4 @@ export default class Main extends Component{
       </Container>
   )
   }
-}
-
-Main.navigationOptions = {
-  title: 'Users'
 }
--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -4,6 +4,6 @@ import React from 'react'
 import {View} from 'react-native'
 
 
-export default function User(){
+export default function User(props){
   return <View />
 }
